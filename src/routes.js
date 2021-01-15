4d125788e9df52dcc95cf08d3f951239128d0998
Adam Vollrath <adam@idyl.tech>
2021-01-15 02:37:57 -0600

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express 	= require( 'express' )
 const appRouter = express.Router()
 
 appRouter.get( '/*', function( req, res ) {
-  res.send( '<h1>Hello, 2021!</h1>' )
+  res.send( '<h1>Hello, it\'s still 2021!</h1>' )
 })
 
 module.exports = appRouter
